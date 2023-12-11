6af96dc3ade6dbced37ba96da7ee4298010cc2d3
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-12-11 13:56:51 +0100

    home edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+//import { Route, Routes } from 'react-router-dom'
+import Homepage from "./components/Homepage"
+import {BrowserRouter as Router, Route, Routes}from "react-router-dom"
 
 function App() {
   
