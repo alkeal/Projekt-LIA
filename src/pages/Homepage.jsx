94797dd809417e9d4564663bc3f3598e5be0b254
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-12-13 16:09:37 +0100

    layout edited

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,65 +1,57 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import sampleImage from "../assets/sampleimage.jpg";
 import "../styles/Homepage.css";
 
 function Homepage() {
-
-  
   return (
-
-    
-    
     <div className="homepage">
+      <div className="top-section">
+        <div className="name-section">
+          <h2>MIA</h2>
+          <h1>HADADI</h1>
+        </div>
 
-      <div className="name-section">
-      <h2>MIA</h2>
-      <h1>HADADI</h1>
+        <div className="gallery-section">
+          {/* Animation to the right*/}
+          <img src={sampleImage} alt="SAMPLE" className="sample-image" />
 
-      </div>
-
-      <div className="gallery-section">
-        
-         {/* Animation to the right*/}
-
-         {/* A button with an arrow in the animation that says 'View Gallery' */}
-      
+          {/* A button with an arrow in the animation that says 'View Gallery' */}
+        </div>
       </div>
 
       <div className="about-section">
+        {/* Images */}
 
-         {/* Images */}
+        {/* Title with text about Mia */}
+        <h1>ABOUT</h1>
 
-          {/* Title with text about Mia */}
-
-          {/* A button with an arrow that says 'Read More' */}
-
+        {/* A button with an arrow that says 'Read More' */}
       </div>
 
       <div className="findme-section">
+        {/* Title */}
+        <h1>FIND ME</h1>
 
-         {/* Title */}
+        {/* Name of Salon displayed over a faded map */}
 
-          {/* Name of Salon displayed over a faded map */}
-
-           {/* A button with an arrow that says 'View Map' */}
-
+        {/* A button with an arrow that says 'View Map' */}
       </div>
 
       <div className="booking-section">
+        {/* Title with text */}
+        <h1>BOOK NOW</h1>
 
-         {/* Title with text */}
+        {/* Heading with text*/}
 
-          {/* Heading with text*/}
+        {/* Images */}
 
-           {/* Images */}
+        {/* Heading with text */}
 
-           {/* Heading with text */}
-
-           {/* A button with an arrow that says 'Book Now' */}
-
+        {/* A button with an arrow that says 'Book Now' */}
       </div>
 
-       {/* Footer */}
+      {/* Footer */}
     </div>
   );
 }
