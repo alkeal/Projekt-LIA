2e5a2c65519789ae8bd3e5d2994ea970fa9802e4
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-12-15 15:03:53 +0100

    find me map added.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -42,12 +42,22 @@ function Homepage() {
       </div>
 
       <div className="findme-section">
+        <div className="map-container">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2036.0489424791488!2d18.0821419!3d59.315439700000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f77f9a049f4b5%3A0xb2e5701445562583!2sFolkungagatan%2089%2C%20116%2030%20Stockholm!5e0!3m2!1sen!2sse!4v1702646677208!5m2!1sen!2sse"
+            width="1170"
+            height="465"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+          <div className="map-overlay"></div>
+        </div>
         <div className="findme-content">
           <h1>FIND ME</h1>
-          <p>Name of Place</p>
+          <h2>Name of Place</h2>
           <button id="btn-findme">VIEW MAP →</button>
         </div>
-      
       </div>
 
       <div className="booking-section">
