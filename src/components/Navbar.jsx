a3972ad8987ffb8ba654059fa2bbf46050148a6d
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-12-15 15:06:54 +0100

    translate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import {Link} from "react-router-dom"
 import React, { useState } from 'react'
 import '../styles/Navbar.css'
+import { useTranslation } from 'react-i18next';
 
 
 export default function Navbar(){
 
     const [openLinks, setOpenLinks] = useState(false)
+    const { t, i18n } = useTranslation();
+
+    const swapLanguage = (langaugeCode) => {
+        i18n.changeLanguage(langaugeCode);
+      };
 
     return(
 
@@ -15,16 +21,24 @@ export default function Navbar(){
     <div className="rightSide" id={openLinks ? "open" : "close"}>
       <div className="hiddenLinks">
 
-     <li><Link  to="/Homepage">HEM</Link></li>
+     <li><Link  to="/Homepage">{t('HEM')}</Link></li>
 
      <li> <Link to="/Gallerypage">{t('GALLERI')}</Link></li>
 
-     <li><Link to="/Infopage">OM MIG</Link></li>
+     <li><Link to="/Infopage">{t('OM MIG')}</Link></li>
 
-     <li><Link to="/Infopage">HITTA HIT</Link></li>
+     <li><Link to="/Infopage">{t('HITTA HIT')}</Link></li>
 
-     <li><Link to="/Bookingpage"><div className="book">BOKA TID</div></Link></li>
-
+     <li><Link to="/Bookingpage"><div className="book">{t('BOKA TID')}</div></Link></li>
+     <li className="Sweden">
+    <img src="./src/assets/Sweden.png" alt="Svenska" onClick={() => swapLanguage('se')} />
+    </li>
+    <li className="UnitedKingdom">
+    <img src="./src/assets/UnitedKingdom.png" alt="English" onClick={() => swapLanguage('en')} />
+    </li>
+    <li className="France">
+    <img src="./src/assets/France.png" alt="French" onClick={() => swapLanguage('fr')} />
+    </li>
            </div>
       </div>
 </ul>
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { HashRouter as Router } from "react-router-dom";
+import {I18nextProvider} from "react-i18next";
+import i18next from "i18next";
+
+
+i18next.init({
+  interpolation: { escapeValue: false }, 
+});
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    
+     <I18nextProvider i18n={i18next}>
       <App />
-    
+      </I18nextProvider>
   </React.StrictMode>
 );
