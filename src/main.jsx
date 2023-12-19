62f5fe37795038198f0e3f10c608f522b14c7131
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-12-19 17:33:55 +0100

    import

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import i18n from "./translate/i18n.js"; // Byt ut mot sökvägen till din i18n-k
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <I18nextProvider i18n={i18next}>
+     <I18nextProvider i18n={i18n}>
       <App />
       </I18nextProvider>
   </React.StrictMode>
