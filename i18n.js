cf62b224b42ce2296cae63d249de71e59de22e5e
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2023-12-19 17:05:13 +0100

    text indent

--- i18n.js
+++ i18n.js
@@ -1,39 +1,37 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import enTranslations from './translate/en.json';
-import svTranslations from './translate/sv.json';
-import frTranslations from './translate/fr.json';
+import enTranslations from "./translate/en.json";
+import svTranslations from "./translate/sv.json";
+import frTranslations from "./translate/fr.json";
 
 const resources = {
   en: {
     translation: {
       // engelska översättningar här
-      enTranslations
+      enTranslations,
     },
   },
   sv: {
     translation: {
       // svenska översättningar här
-      svTranslations
+      svTranslations,
     },
   },
   fr: {
     translation: {
       // franska översättningar här
-      frTranslations
+      frTranslations,
     },
   },
 };
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng: "sv", // standard språk
-    fallbackLng: "sv", // vilket språk att använda om det saknas översättningar
-    interpolation: {
-      escapeValue: false, // reagerar redan på xss
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "sv", // standard språk
+  fallbackLng: "sv", // vilket språk att använda om det saknas översättningar
+  interpolation: {
+    escapeValue: false, // reagerar redan på xss
+  },
+});
 
 export default i18n;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,57 @@
-import {Link} from "react-router-dom"
-import React, { useState } from 'react'
-import '../styles/Navbar.css'
-import { useTranslation } from 'react-i18next';
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import "../styles/Navbar.css";
+import { useTranslation } from "react-i18next";
 
+export default function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+  const { t, i18n } = useTranslation();
 
-export default function Navbar(){
+  const swapLanguage = (languageCode) => {
+    console.log(`Changing language to ${languageCode}`);
+    i18n.changeLanguage(languageCode);
+  };
 
-    const [openLinks, setOpenLinks] = useState(false)
-    const { t, i18n } = useTranslation();
+  return (
+    <div className="Navbar">
+      <ul>
+        <div className="rightSide" id={openLinks ? "open" : "close"}>
+          <div className="hiddenLinks">
+            <li>
+              <Link to="/Homepage">{t("HEM")}</Link>
+            </li>
 
-    const swapLanguage = (langaugeCode) => {
-        i18n.changeLanguage(langaugeCode);
-      };
+            <li>
+              {" "}
+              <Link to="/Gallerypage">{t("GALLERI")}</Link>
+            </li>
 
-    return(
+            <li>
+              <Link to="/Infopage">{t("OM MIG")}</Link>
+            </li>
 
-        <div className="Navbar">
+            <li>
+              <Link to="/Infopage">{t("HITTA HIT")}</Link>
+            </li>
 
- <ul>
-    <div className="rightSide" id={openLinks ? "open" : "close"}>
-      <div className="hiddenLinks">
+            <li>
+              <Link to="/Bookingpage">
+                <div className="book">{t("BOKA TID")}</div>
+              </Link>
+            </li>
 
-     <li><Link  to="/Homepage">{t('HEM')}</Link></li>
-
-     <li> <Link to="/Gallerypage">{t('GALLERI')}</Link></li>
-
-     <li><Link to="/Infopage">{t('OM MIG')}</Link></li>
-
-     <li><Link to="/Infopage">{t('HITTA HIT')}</Link></li>
-
-     <li><Link to="/Bookingpage"><div className="book">{t('BOKA TID')}</div></Link></li>
-     <li className="Sweden" onClick={() => swapLanguage('sv')}>
-  <img src="./src/assets/Sweden.png" alt="Svenska" />
-     </li>
-     <li className="UnitedKingdom" onClick={() => swapLanguage('en')}>
-  <img src="./src/assets/UnitedKingdom.png" alt="English" />
-     </li>
-     <li className="France" onClick={() => swapLanguage('fr')}>
-  <img src="./src/assets/France.png" alt="French" />
-    </li>
-           </div>
-      </div>
-</ul>
-
- </div>
-
-
-
-    )
-
-
+            <li className="Sweden" onClick={() => swapLanguage("sv")}>
+              <img src="./src/assets/Sweden.png" alt="Svenska" />
+            </li>
+            <li className="UnitedKingdom" onClick={() => swapLanguage("en")}>
+              <img src="./src/assets/UnitedKingdom.png" alt="English" />
+            </li>
+            <li className="France" onClick={() => swapLanguage("fr")}>
+              <img src="./src/assets/France.png" alt="French" />
+            </li>
+          </div>
+        </div>
+      </ul>
+    </div>
+  );
 }
