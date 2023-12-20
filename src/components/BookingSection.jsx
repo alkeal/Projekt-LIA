c9743403a8172d3ebec85ba7eb56cfd16c34c857
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-12-20 13:03:50 +0100

    Booking component with css added

---
+++ src/components/BookingSection.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/BookingSection.css";
+
+function BookingSection() {
+  return (
+    <div className="booking-section">
+      {/* Title with text */}
+      <h1>BOOK NOW</h1>
+
+      {/* Heading with text*/}
+
+      {/* Images */}
+
+      {/* Heading with text */}
+
+      {/* A button with an arrow that says 'Book Now' */}
+    </div>
+  );
+}
+
+export default BookingSection;
