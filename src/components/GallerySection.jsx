978b059bfaf8ea11fc3cdbc3d623aa76da6bb7c2
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-12-20 13:02:03 +0100

    Gallery component created with css

---
+++ src/components/GallerySection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import sampleImage from "../assets/sampleimage.jpg";
+import "../styles/GallerySection.css";
+
+function GallerySection() {
+  return (
+    <div className="gallery-section">
+      {/* Animation to the right*/}
+      <img src={sampleImage} alt="SAMPLE" className="sample-image" />
+
+      {/* A button with an arrow in the animation that says 'View Gallery' */}
+      <button id="btn-gallery">VIEW GALLERY → </button>
+    </div>
+  );
+}
+
+export default GallerySection;
