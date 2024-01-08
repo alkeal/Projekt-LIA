50eed3f8e7cd26f248507953934f53a9e4fea6bc
alkeal <yhma22.alkeal@folkuniversitetet.nu>
2024-01-08 12:17:22 +0100

    test av footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,21 @@
-import {Link} from "react-router-dom"
-import React, { useState } from 'react'
-import '../styles/components/Footer.css'
+
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import "../styles/components/Footer.css";
+import { useTranslation } from "react-i18next";
+
+export default function Footer() {
+  const [openLinks, setOpenLinks] = useState(false);
+  const { t, i18n } = useTranslation();
+
+  const swapLanguage = (languageCode) => {
+    console.log(`Changing language to ${languageCode}`);
+    i18n.changeLanguage(languageCode);
+  };
+return (
+
+<div></div>
 
 
-export default function Footer(){
 
-
-
-
-
-
-}
+);}
